0b47ce6a4a9ca429173e25500de718df0fe36e88
Maxime Mangel <mangel.maxime@protonmail.com>
2022-09-21 15:15:13 +0200

    Clean up imports

--- src/Components/PackageHome/Readme.fs
+++ src/Components/PackageHome/Readme.fs
@@ -2,14 +2,8 @@ module Fable.Packages.Components.PackageHome.Readme
 
 open Fable.Core.JsInterop
 open Feliz
-open Feliz.UseDeferred
 open Feliz.Bulma
 open Fable.Packages.Components
-open Fable.Packages.Types
-open Feliz.ReactMarkdown
-
-let private remarkGfm: obj =
-    import "default" "remark-gfm"
 
 // Workaround to have React-refresh working
 // I need to open an issue on react-refresh to see if they can improve the detection
@@ -18,7 +12,6 @@ emitJsStatement () "import React from \"react\""
 [<ReactComponent(import="default", from="../MarkdownContent.jsx")>]
 let MarkdownContent(content : string) =
     React.imported()
-
 
 type Components with
 
